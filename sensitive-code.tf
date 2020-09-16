c65feb48322422f4ac9d065f70e48adf5f8ce119
Vinil Mehta <vinci141@gmail.com>
2020-09-16 10:24:20 +0530

    Update sensitive-code.tf

--- sensitive-code.tf
+++ sensitive-code.tf
@@ -8,7 +8,7 @@ locals {
 # output "db_password"{
 #   value = local.db_password
 # }
-#
+
 # Apply complete! Resources: 0 added, 0 changed, 0 destroyed.
 #
 # Outputs:
@@ -16,16 +16,13 @@ locals {
 # db_password = {
 #   "admin" = "password"
 # }
-
-#
 #CODE 2 :Below code will NOT display sensitive information on console & on cloud.
 output "db_password"{
   value = local.db_password
   sensitive = true
 }
-#
-# C:\Users\vinilm\OneDrive - AMDOCS\Backup Folders\Documents\CloudCertifications\Terraform\Terraform-project\terraform-sensitive-data>terraform apply
-#
+
+
 # Apply complete! Resources: 0 added, 0 changed, 0 destroyed.
 #
 # Outputs:
